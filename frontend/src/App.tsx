7081d0ec549e1fff1517ed2597098715d084a2ac
Kauan Linhares <kauan-linhares@hotmail.com>
2022-03-10 01:38:40 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import Navbar from "./components/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>                                           {/* inicia configuração das rotas */}
+      <Navbar />                                              {/* Toda pagina terá a Navbar */}
+      <Routes>                                                {/* Configura rota por rota */}
+        <Route path="/" element={<Listing />} />              {/* A rota do caminho raiz (/) abre a página Listing */}
+        <Route path="/form">                                 
+          <Route path=":movieId" element={<Form />} />        {/* A rota do caminho /form/id abre a página Form */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
