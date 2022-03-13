cbdb2bd199749787bee9bc72eb3a89db870e1a52
Kauan Linhares <kauan-linhares@hotmail.com>
2022-03-13 19:38:13 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,26 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {                                         //.then() executa quando a requisicao volta
-            console.log(response.data)
-        });     
+    const [pageNumber, setPageNumber] = useState(0); // useState: manter estado no componente
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {                         //.then() executa quando a requisicao volta
+            const data = response.data as MoviePage;    
+            console.log(data)                           
+            setPageNumber(data.number)
+        });
+    }, []); // sempre que o objeto "[]" mudar ele vai executar a funcao de novo
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
