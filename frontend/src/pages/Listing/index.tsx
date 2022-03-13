33be75c29f8d41c2971b5915e81e20c0c996822b
Kauan Linhares <kauan-linhares@hotmail.com>
2022-03-13 20:11:17 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`) // sort: ordena por titulo
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`) // sort: ordena por titulo
             .then(response => {                         //.then() executa quando a requisicao volta
                 const data = response.data as MoviePage;
                 setPage(data)
